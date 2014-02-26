c51f562fa7bd8d98b325cb1e5a6576859645cc31
steffan <steffanhalv@gmail.com>
2014-02-26 13:26:23 +0100

    Added yuidoc integration in 'scripts' folder and added language parameter in albumGet

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,18 @@
 'use strict';
 
+/**
+ *  This modul is used for debugging the bmm Library
+ *  All routing and server connection is configured here
+ *  
+ *  @module bmmDebug
+ *  @requires ngResource
+ *  @requires ngSanitize
+ *  @requires ngRoute
+ *  @requires bmmLibApp
+ *  @requires ui.sortable
+ *  @requires angularTreeview
+ */
+
 angular.module('bmmDebug', [
   'ngResource',
   'ngSanitize',
@@ -8,7 +21,7 @@ angular.module('bmmDebug', [
   'ui.sortable',
   'angularTreeview'
 ]).run(['$route', 'bmmApi', function($route, bmmApi)  {
-    
+
     bmmApi.serverUrl('https://'+window.location.hostname+'/bmm/api/web/app_dev.php/');
     
     bmmApi.user().done(function() {
---
+++ app/scripts/out/api.js
@@ -0,0 +1,15 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [],
+    "modules": [
+        "bmmDebug"
+    ],
+    "allModules": [
+        {
+            "displayName": "bmmDebug",
+            "name": "bmmDebug",
+            "description": "This modul is used for debugging the bmm Library"
+        }
+    ]
+} };
+});
