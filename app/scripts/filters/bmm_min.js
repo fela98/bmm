4233ede163bd1b3079e3ac7f8cf8b5c993371d3e
steffan <steffanhalv@gmail.com>
2014-03-10 16:33:56 +0100

    Fixes to bmmMin filter

--- app/scripts/filters/bmm_min.js
+++ app/scripts/filters/bmm_min.js
@@ -4,6 +4,10 @@ angular.module('bmmLibApp')
   .filter('bmmMin', function () {
     return function (str, size) {
 
+      if (typeof size==='undefined') {
+        size = 15;
+      }
+
       if (str.length>size) {
         str = str.substring(0,size)+'...';
       }
